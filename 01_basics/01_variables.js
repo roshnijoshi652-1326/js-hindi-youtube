937368356f618f48406959eb5ea8abb28a332735
roshnijoshi652-1326 <roshnijoshi652@gmail.com>
2026-04-03 19:08:38 +0000

    datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountID =144553
+let accountEmail ="roshni@google.com"
+var accountPassword="12345"
+ let acoountCity = "Jaipur"
+ let accountState;
+accountEmail="rj@rj.com"
+accountPassword="2121212121"
+accountCity="Bengaluru"
+console.log(accountID);
+/*
+Prefer not to use var
+because of issue in block and functional scope
+*/
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState])
