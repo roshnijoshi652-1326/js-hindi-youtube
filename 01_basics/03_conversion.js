42cce0344bd85902c5eb3923bbbbfe27ffded305
roshnijoshi652-1326 <roshnijoshi652@gmail.com>
2026-04-04 08:41:52 +0000

    conversion part 2

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -3,21 +3,59 @@
 /*let score = true = it will written 1 or it will written 0 in false condition*/
 //"33"=>33
 //"33abc"=>NaN "not a number"
-let score = undefined
-console.log(typeof score);
-console.log(typeof (score));
+//let score = undefined
+//console.log(typeof score);
+//console.log(typeof (score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 //let IsloggedIn =1
 //let booleanIsLoggedIn = Boolean(isLoggedIn)
 //console.log(booleanIsLoggedIn);
 //1=>true;0=>false
 //""=> false
 //"hitesh"=>true
-let someNumber =33
+//let someNumber =33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//****************operation*******************//
+//let value = 3
+//let negValue = -value
+//console.log(negValue);
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+//let str1 ="roshni"
+//let str2 =" joshi"
+//let str3 = str1 + str2
+//console.log(str3);
+
+//console.log("1"+2);
+//console.log(1+"2");
+//console.log("1"+2+2);
+//console.log(1+2+"2");
+//console.log (3+4*5%3);= don't do this
+//console.log(+true);
+//console.log(+"");
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+//console.log("2">1);
+//console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined == 0);
+
+// ===
+console.log("2"===2);
