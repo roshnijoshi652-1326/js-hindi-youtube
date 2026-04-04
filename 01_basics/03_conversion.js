5b1bd9c252f42af235e9eb23627f806c79a7c667
roshnijoshi652-1326 <roshnijoshi652@gmail.com>
2026-04-04 07:16:25 +0000

    conversion of datatype

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,23 @@
+/*let score = null= in this case value is written 0;*/
+/*let score = undefined = in this caase value is written NaN*/
+/*let score = true = it will written 1 or it will written 0 in false condition*/
+//"33"=>33
+//"33abc"=>NaN "not a number"
+let score = undefined
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+//let IsloggedIn =1
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+//1=>true;0=>false
+//""=> false
+//"hitesh"=>true
+let someNumber =33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
